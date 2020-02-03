08759f26cd9a24d659ab91a5ca7e52df85041bc3
NavyaTatikonda <inavyatatikonda@gmail.com>
2020-02-03 09:31:37 +0530

    Update disemvowel.py

--- disemvowel.py
+++ disemvowel.py
@@ -1,5 +1,18 @@
+#SOLUTION 1-
 def disemvowel(string):
     for i in 'aeiouAEIOU':
         string=string.replace(i,"")
+    
+    return string
+
+
+
+#SOLUTION 2-
+
+def disemvowel(string):
+    vowels=('a','e','i','o','u','A','E','I','O','U')
+    for i in string:
+        if i in vowels:
+            string=string.replace(i,"")
     
     return string
